ac7d22ec1c6498cf0403a7b59e3cb64182b2f1be
Alistair Lacy <bundyza@gmail.com>
2022-07-08 17:29:10 +0200

    Enhanced output logging

--- git/gitpull.py
+++ git/gitpull.py
@@ -1,11 +1,40 @@
 #pip install gitpython
 import git
+from typing import Union
 import os
 import os.path
 
 class PullProgressPrinter(git.RemoteProgress):
-    def update(self, op_code, cur_count, max_count=None, message=''):
-        print(op_code, cur_count, max_count, cur_count / (max_count or 100.0), message or "NO MESSAGE")
+    
+    code = {
+        4: 'Counting object(s)',
+        8: 'Compressing object(s)',
+        32: 'Receiving object(s)',
+        64: 'Resolving delta(s)'
+    }
+
+    def _toggle(self, op_code, bit_num):
+        return (op_code ^ (1 << (bit_num- 1)))
+    
+    def _decode_opcode(self, op_code):
+        if op_code & 1:
+            return (1, self._toggle(op_code, 1))
+
+        if op_code & 2:
+            return (2, self._toggle(op_code, 2))
+
+        return (0, op_code)
+
+    def update(self, op_code: int, cur_count: Union[str, float], max_count: Union[str, float, None] = None, message: str = ''):
+        start_end, op_code = self._decode_opcode(op_code)
+
+        operation = self.code.get(op_code, op_code)
+        percentage = cur_count / (max_count or 100.0) 
+
+        print(f"{operation} item {cur_count} of {max_count} ({percentage * 100} %)")
+
+        if message:
+            print(message)
 
 def list_dirs(folder):
     return [
