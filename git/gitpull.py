fe3214e0e868cb24f91f63040dce1ae297f4e558
Alistair Lacy <bundyza@gmail.com>
2022-07-18 11:13:59 +0200

    Updated summary formatting

--- git/gitpull.py
+++ git/gitpull.py
@@ -5,7 +5,7 @@ import os
 import os.path
 
 class PullProgressPrinter(git.RemoteProgress):
-    
+
     code = {
         4: 'Counting object(s)',
         8: 'Compressing object(s)',
@@ -15,7 +15,7 @@ class PullProgressPrinter(git.RemoteProgress):
 
     def _toggle(self, op_code, bit_num):
         return (op_code ^ (1 << (bit_num - 1)))
-    
+
     def _decode_opcode(self, op_code):
         if op_code & 1:
             return (1, self._toggle(op_code, 1))
@@ -32,45 +32,59 @@ class PullProgressPrinter(git.RemoteProgress):
         percentage = cur_count / (max_count or 100.0)
 
         if start_end != 2:
-            print(f"{operation} - item {cur_count} of {max_count} ({round(percentage * 100, 2)} %)")
+            if max_count:
+                print(f"{operation} - item {cur_count} of {max_count} ({round(percentage * 100, 2)} %) {message}")
+            else:
+                print(f"{operation} - item {cur_count} {message}")
+
         else:
             print(f"{operation} - Done.")
-
-        if message:
-            print(message)
 
 def list_dirs(folder):
     return [
         d for d in (os.path.join(folder, d1) for d1 in os.listdir(folder))
         if os.path.isdir(d)
     ]
-    
+
 def git_pull(subdir):
     root = os.getcwd()
-    
+
     try:
         if not os.path.exists(subdir):
             return 'Skipped'
-        
+
         os.chdir(subdir)
-        
+
         if not os.path.exists('.git'):
             return 'Skipped'
-        
-        location = os.path.abspath(subdir)
-        print(f'Updating {location}...')            
+
+        location = os.path.abspath('.')
+        print(f'Updating {location}...')
         repo = git.Repo('.')
         origin = repo.remotes.origin
         origin.pull(progress=PullProgressPrinter(), prune=True)
-        print(f'Updated.')
+        print(f'{location} Updated.')
         return 'Updated'
-    
+
     except Exception as e:
-        print(f'Update error: {e}')
+        print(f'Error: {e}')
         return 'Error'
-    
+
     finally:
-        os.chdir(root)    
+        os.chdir(root)
+        
+def print_summary(summary):
+    print()
+    print('GitPull Summary:')
+    print('----------------')
+    
+    length = len(max((s[0] for s in summary), key=len)) + 3
+    
+    for s in summary:
+        repo = s[0] + ':'
+        line = f'{repo:{length}} {s[1]}'
+        print(line)        
+    
 
 def main():
     summary = []
@@ -78,13 +92,7 @@ def main():
     for dir in list_dirs(os.path.curdir):
         summary.append((dir, git_pull(dir)))
 
-    print()
-    print('Summary:')
-    print('------------------------------')
-    
-    for s in summary:
-        print(f'{s[0]}: {s[1]}.')        
+    print_summary(summary)
 
 if __name__ == '__main__':
     main()
-    
